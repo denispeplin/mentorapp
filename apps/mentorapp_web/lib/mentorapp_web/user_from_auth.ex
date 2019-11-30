04bb89f517e5c04f023314952e8e782d32b4aaaf
denispeplin <denis.peplin@gmail.com>
2019-11-30 22:08:18 +0400

    Get more information from Github.

--- apps/mentorapp_web/lib/mentorapp_web/user_from_auth.ex
+++ apps/mentorapp_web/lib/mentorapp_web/user_from_auth.ex
@@ -10,11 +10,15 @@ defmodule UserFromAuth do
     {:ok, basic_info(auth)}
   end
 
-  defp basic_info(auth) do
-    %{id: auth.uid, name: name_from_auth(auth), avatar: avatar_from_auth(auth)}
+  defp basic_info(%{info: info} = auth) do
+    %{
+      id: auth.uid,
+      name: info.name,
+      nickname: info.nickname,
+      description: info.description,
+      avatar: avatar_from_auth(auth)
+    }
   end
 
   defp avatar_from_auth(%{info: %{urls: %{avatar_url: image}}}), do: image
-
-  defp name_from_auth(auth), do: auth.info.name
 end
